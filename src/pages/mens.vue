c44f8843b0e44470dbd95fb35cf22d368593c9d5
BriqueAuBob <briqueaubob@gmail.com>
2023-12-22 12:20:50 +0100

    hommes

--- src/pages/mens.vue
+++ src/pages/mens.vue
@@ -12,7 +12,7 @@
       class="w-1/2 mx-auto rounded-2xl my-8 shadow-md"
     />
     <p>
-      En tant que cultivateur, Darshan est confronté à des conditions de travail
+      En tant que cultivateur, Apu est confronté à des conditions de travail
       extrêmement précaires. Pour traiter ses champs de coton, il se voit
       contraint d'utiliser un mélange d'insecticides, comprenant de l'Ethion, un
       produit interdit dans l'Union européenne en raison de sa dangerosité, et
@@ -21,23 +21,22 @@
       combinaisons ou des gants, exposant ainsi sa santé et celle de sa famille
       à des risques graves. La nécessité de recourir à ces produits toxiques est
       impérative, car sans eux, la production de coton, principale source de
-      revenu de Darshan, serait compromise. La peur règne parmi les
-      cultivateurs, conscient des réactions graves voire mortelles que ces
-      substances peuvent provoquer. Malgré ces dangers, Darshan n’a pas d’autres
-      choix que d’impliquer sa famille dans son exploitation, faisant participer
-      même son jeune fils. Le coton ainsi récolté est vendu à des entreprises
-      multinationales à des prix dérisoires, telles qu'Enik. Cette réalité
-      économique laisse à Darshan un revenu mensuel de seulement 60 euros, soit
-      plus de la moitié du revenu moyen en Inde. Avec ces revenus plus que
-      modestes, il est difficile pour lui de subvenir aux besoins de sa famille.
-      De plus, son fils est touché par une maladie grave. Son foie étant
-      défaillant, il ne remplit plus sa fonction, et il nécessite une
-      transplantation coûteuse de 37 000 euros, une somme inaccessible pour
-      cette famille. Tragiquement, la petite fille de Darshan souffre également
-      d'une maladie hépatique. Darshan le sait, son fils et sa fille ont une
-      épée de Damoclès au-dessus de leurs têtes, pourtant, il ne peut pas non
-      plus arrêter d’utiliser des pesticides. Le médecin de leur village affirme
-      que cela est dû aux pesticides utilisés.
+      revenu de Apu, serait compromise. La peur règne parmi les cultivateurs,
+      conscient des réactions graves voire mortelles que ces substances peuvent
+      provoquer. Malgré ces dangers, Apu n’a pas d’autres choix que d’impliquer
+      sa famille dans son exploitation, faisant participer même son jeune fils.
+      Le coton ainsi récolté est vendu à des entreprises multinationales à des
+      prix dérisoires, telles qu'Enik. Cette réalité économique laisse à Apu un
+      revenu mensuel de seulement 60 euros, soit plus de la moitié du revenu
+      moyen en Inde. Avec ces revenus plus que modestes, il est difficile pour
+      lui de subvenir aux besoins de sa famille. De plus, son fils est touché
+      par une maladie grave. Son foie étant défaillant, il ne remplit plus sa
+      fonction, et il nécessite une transplantation coûteuse de 37 000 euros,
+      une somme inaccessible pour cette famille. Tragiquement, la petite fille
+      de Apu souffre également d'une maladie hépatique. Apu le sait, son fils et
+      sa fille ont une épée de Damoclès au-dessus de leurs têtes, pourtant, il
+      ne peut pas non plus arrêter d’utiliser des pesticides. Le médecin de leur
+      village affirme que cela est dû aux pesticides utilisés.
       <br /><br />
       Malheureusement, leur situation familiale n'est pas un cas unique, car la
       plupart des familles des cultivateurs de la région connaissent au moins un
