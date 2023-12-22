6ec7e2e8e1990a5f640ebc9e2187824c9d88bfca
BriqueAuBob <briqueaubob@gmail.com>
2023-12-22 11:11:51 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/ekin",
-  build: {
-    rollupOptions: {
-      external: ["vue", "/ekin/src/main.js"],
-    },
-  },
+  base: "./",
 });
